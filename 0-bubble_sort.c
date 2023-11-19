f6157875266f9fdede334b76b4a74741ec1a9520
xclusivesmash <sbumatshiane916@gmail.com>
2023-11-19 10:59:45 +0200

    Updated code logic

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,32 @@
 #include "sort.h"
 
 /**
- * bubble_sort - implements bubble-sort algorithm.
+ * bubble_sort - sorts @array in ascending order.
  * @array: input array.
  * @size: size of @array.
  * Return: void.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t len = size, i;
-	int tmp;
-	bool flag = false;
+	size_t i, len;
+	int check = 0, temp;
 
-	if (array == NULL || size <= 2)
+	/* CHECK IF ARRAY IS NULLL OR SIZE IS LESS THAN 2 */
+	if (array == NULL || size < 2)
 		return;
-	while (flag == false)
+	len = size;
+	while (check == 0)
 	{
-		flag = true;
+		check = 1;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				tmp = array[i];
+				temp = array[i];
 				array[i] = array[i + 1];
-				array[i + 1] = tmp;
+				array[i + 1] = temp;
 				print_array(array, size);
-				flag = false;
+				check = 0;
 			}
 		}
 		len = len - 1;
