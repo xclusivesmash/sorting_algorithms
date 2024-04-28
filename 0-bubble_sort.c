6c7090aa7c359eae4b9be0545796b82323b10b34
xclusivesmash <sbumatshiane916@gmail.com>
2024-04-28 03:11:09 -0700

    Initial commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,34 @@
 #include "sort.h"
 
-
 /**
-* bubble_sort - sorts an array in ascending order.
-* @array: To be sorted.
-* @size: size of @array.
-* Return: void.
-*/
+ * bubble_sort - sorts @array in ascending order.
+ * @array: input array.
+ * @size: size of @array.
+ * Return: void.
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, len;
-    int check = 0, temp;
+	size_t i, len;
+	int check = 0, temp;
 
-
-    /* DO NOT SORT UNDER THIS COND. */
-    if (array == NULL || size <= 2)
-        return;
-    len = size;
-    while (check == 0)
-    {
-        check = 1;
-        for (i = 0; i < (len - 1); i++)
-        {
-            if (array[i + 1] < array[i])
-            { /* THAT'S WHEN WE DO THE SWAPPING */
-                temp = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = temp;
-                /* print array */
-                print_array(array, size);
-                check = 0;
-            }
-        }
-        len = len - 1;
-    }
+	/* CHECK IF ARRAY IS NULLL OR SIZE IS LESS THAN 2 */
+	if (array == NULL || size < 2)
+		return;
+	len = size;
+	while (check == 0)
+	{
+		check = 1;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				check = 0;
+			}
+		}
+		len = len - 1;
+	}
 }
