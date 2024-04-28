40e3497e19efcc64239a66b8c1c99ff3b75e78ae
xclusivesmash <sbumatshiane916@gmail.com>
2024-04-28 03:12:13 -0700

    Added header file

--- sort.h
+++ sort.h
@@ -1,7 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
 
-/* structures to be used */
+/* Standard header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Structure definitions */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,14 +16,68 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* function prototypes */
+/**
+ * enum kind_e - defining parts of a deck.
+ * @SPADE: spade.
+ * @HEART: heart.
+ * @CLUB: club.
+ * @DIAMOND: diamond type.
+ * Description: None.
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 #endif /* SORT_H */
